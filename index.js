2a47f9ae4964c0eca2186fc13075fd3dbaf3dbbb
crayjay-source <jadenbgcworcester@gmail.com>
2025-08-06 14:52:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("jaden")
+background(0, 255, 162);
+var x = 200;
+var y = 250;
+
+noStroke();
+fill(30, 204, 91); // a nice froggy green!
+
+ellipse(x, y, 200, 100); // face
+ellipse(x - 50, y - 50, 40, 40); // left eye socket
+ellipse(x + 50, y - 50, 40, 40); // right eye socket
+
+fill(255, 255, 255); // for the whites of the eyes!
+ellipse(x - 50, y - 50, 30, 30); // left eyeball
+ellipse(x + 50, y - 50, 30, 30); // right eyeball
+fill(0, 0, 0);
+ellipse(x, y, 156, 73);
+rect(x - 61, y - 62, 15, 15);
+rect(x + 39, y - 60, 15, 15);
