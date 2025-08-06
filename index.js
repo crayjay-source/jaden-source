308e98f75526d59c34b4745c8c688e555c42d72a
crayjay-source <jadenbgcworcester@gmail.com>
2025-08-06 15:09:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 console.log("jaden")
-var xPos = 200;
-var yPos = 200;
-var W = 10;
-var H = 10;
-draw = function() {
-    background(29, 40, 115);
-    fill(255, 242, 0);
-    ellipse(xPos, yPos, W, H);
-    W+=1; 
-    H+=1;
+
